2037fd87fd45f6dd0a72276fff0622fa552e515b
Himanshu <himanshugoswami.12feb@gmail.com>
2022-05-08 17:11:45 +0530

    Added kitty configuration

--- zsh/nnn.zsh
+++ zsh/nnn.zsh
@@ -6,7 +6,9 @@ export EDITOR=vim
 # export PDF=vim -- Find command for pdf opener
 # Here f is the key to open finder
 export NNN_PLUG='t:preview-tabbed;v:imgview;w:wallpaper' 
-export NNN_FCOLORS='c1e2252e006033f7c6d6abc4'
 export NNN_COLORS="2136" # This will have different color for each 1, 2, 3, 4
 export NNN_FIFO=/tmp/nnn.fifo
 export NNN_TMPFILE='~/.config/nnn/.lastd' # always cd on quit
+
+# --- THEMES 
+export NNN_FCOLORS='c1e2252e006033f7c6d6abc4'

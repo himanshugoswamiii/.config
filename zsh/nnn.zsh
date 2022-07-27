27cbe734c2ca17e8f054c46e539afca86ab985da
Himanshu <himanshugoswami.12feb@gmail.com>
2022-07-27 19:56:38 +0530

    Awesome window manager config

--- zsh/nnn.zsh
+++ zsh/nnn.zsh
@@ -4,8 +4,9 @@ export VISUAL=nvim
 # EDITOR will change the batch rename editor from nano to neovim
 export EDITOR=vim
 # export PDF=vim -- Find command for pdf opener
-# Here f is the key to open finder
-export NNN_PLUG='t:preview-tabbed;v:imgview;w:wallpaper' 
+
+# Here t is the key to open preview-tabbed
+export NNN_PLUG='t:preview-tabbed;v:imgview;w:wallpaper;y:x2sel' 
 export NNN_COLORS="2136" # This will have different color for each 1, 2, 3, 4
 export NNN_FIFO=/tmp/nnn.fifo
 export NNN_TMPFILE='~/.config/nnn/.lastd' # always cd on quit

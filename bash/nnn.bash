49d424b75fd5cf4c25eb1f50e1946aa1cb138c29
Himanshu <himanshugoswami.12feb@gmail.com>
2022-10-11 00:41:05 +0530

    qutebrowser config , nnn config

--- bash/nnn.bash
+++ bash/nnn.bash
@@ -1,12 +1,12 @@
 # -------------NNN File Manager----------------------
 
-export NNN_OPTS="Aeo"  # Look at Program Options in the nnn Wiki
+export NNN_OPTS="Ao"  # Look at Program Options in the nnn Wiki
 # A : no dir auto-entering during filter
 # e : text in $VISUAL/$EDITOR/vi
 # o : Open files only on enter. Now `l` won't open files
 # x : Copy selection path to clipboard
 
-export VISUAL=nvim
+#export VISUAL=nvim
 export EDITOR=vim  # EDITOR will change the batch rename editor from nano to vim
 # export PDF=vim -- Find command for pdf opener
 
@@ -14,10 +14,7 @@ export EDITOR=vim  # EDITOR will change the batch rename editor from nano to vim
 export NNN_PLUG='t:preview-tabbed;v:imgview;w:wallpaper;y:x2sel'
 export NNN_COLORS="2136" # This will have different color for each 1, 2, 3, 4
 export NNN_FIFO=/tmp/nnn.fifo
-# export NNN_TMPFILE='/home/himanshu/.config/nnn/.lastd' # always cd on quit
+export NNN_TMPFILE='/home/himanshu/.config/nnn/.lastd' # always cd on quit
 
 # --- THEMES
 export NNN_FCOLORS='c1e2252e006033f7c6d6abc4'
-
-
-
--- zsh/nnn.zsh
+++ zsh/nnn.zsh
@@ -6,15 +6,15 @@ export NNN_OPTS="Aeo"  # Look at Program Options in the nnn Wiki
 # o : Open files only on enter. Now `l` won't open files
 # x : Copy selection path to clipboard
 
-export VISUAL=nvim
+#export VISUAL=nvim
 export EDITOR=vim  # EDITOR will change the batch rename editor from nano to vim
 # export PDF=vim -- Find command for pdf opener
 
 # Here t is the key to open preview-tabbed
-export NNN_PLUG='t:preview-tabbed;v:imgview;w:wallpaper;y:x2sel' 
+export NNN_PLUG='t:preview-tabbed;v:imgview;w:wallpaper;y:x2sel'
 export NNN_COLORS="2136" # This will have different color for each 1, 2, 3, 4
-# export NNN_FIFO=/tmp/nnn.fifo
-# export NNN_TMPFILE='/home/himanshu/.config/nnn/.lastd' # always cd on quit
+export NNN_FIFO=/tmp/nnn.fifo
+export NNN_TMPFILE='/home/himanshu/.config/nnn/.lastd' # always cd on quit
 
-# --- THEMES 
+# --- THEMES
 export NNN_FCOLORS='c1e2252e006033f7c6d6abc4'
